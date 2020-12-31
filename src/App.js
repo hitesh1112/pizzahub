2cc7fbb77d9a322a93f77fbbd5ce5155913c54c3
hitesh1112 <h.95verma@gmail.com>
2020-12-31 15:54:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import Header from './Component/Header';
+import React, { useEffect, useState } from "react";
+import {BrowserRouter as Router,Switch,Route,} from "react-router-dom";
+import Customize from './Component/Customize';
+import Checkout from './Component/Checkout';
 import './App.css';
 
 function App() {
+  const [ingredients, setIngredients] = useState({
+    basil:false,
+    cheese:false,
+    mushroom:false,
+    olive:false,
+    pineapple:false,
+    tomato:false,
+  });
+
+  useEffect(() =>{
+    const data= localStorage.getItem("ingredients");
+    if(data){
+      setIngredients(JSON.parse(data))
+    }
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Customize ingredients={ingredients} setIngredients={setIngredients} />
+          </Route>
+          <Route path="/checkout">
+              {/* <h1>Checkout Hello</h1>  */}
+            <Checkout ingredients={ingredients}/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+        <div style={{padding:'10px 50px',backgroundColor:"black" }}>
+            <div style={{color:"#fff"}}>Pizza Customize</div>
+        </div>
+        </>
+    );
+
+}
+
+export default Header;
